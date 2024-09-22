d16d447edf3e852c010ad63746451221a1a2ae85
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-09-22 21:25:51 +0630

    [feat: model.py updated -> fix the model file path]

--- backend/model.py
+++ backend/model.py
@@ -1,4 +1,14 @@
+import os
+
 import joblib
 
-# Load the Model
-model = joblib.load('C:\\Users\\acer\\Desktop\\Development\\drepression-model\\backend\\xgboost_model.pkl')
+# Define the path to the .pkl file
+model_path = os.path.join('backend', 'xgboost_model.pkl')
+
+# Load the model using joblib
+@st.cache_resource
+def load_model():
+    return joblib.load(model_path)
+
+# Load the model only once when the app starts
+model = load_model()
